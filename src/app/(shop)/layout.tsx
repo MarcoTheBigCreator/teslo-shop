b56b9454940956381785e0523dce804a35d11afa
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-11 13:17:42 -0600

    feat: Add react-icons package to dependencies
    The code changes include adding the react-icons package to the dependencies in package.json. This package provides a collection of popular icons for use in the application.

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,13 @@
+import { TopMenu } from '../../components/ui/top-menu/TopMenu';
 export default function ShopLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="min-h-screen bg-red-500">{children}</main>;
+  return (
+    <main className="min-h-screen">
+      <TopMenu />
+      {children}
+    </main>
+  );
 }
