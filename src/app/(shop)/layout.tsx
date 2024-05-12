930d70ee5d622343b65e998130510d18e5d4ac7c
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-11 21:52:42 -0600

    feat: Add Title component for shop pages
    The code changes include adding a new Title component in the src/components/ui/title directory. This component is used to display the title and subtitle for shop pages. It utilizes the titleFont from the src/config/fonts file and applies specific styling classes for the title and subtitle.

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -7,7 +7,7 @@ export default function ShopLayout({
   return (
     <main className="min-h-screen">
       <TopMenu />
-      {children}
+      <div className="px-0 sm:px-10">{children}</div>
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
+export * from './ui/title/Title';
