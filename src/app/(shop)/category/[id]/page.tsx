52d6bbaf2316b55d48c819415d0a69900d9dba9c
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-11 21:42:47 -0600

    feat: Add category page and handle not found case
    The code changes include adding a new category page for the shop, which displays the category ID. Additionally, a new "not-found" page is added to handle the case when the category ID is "kids". This page shows a custom 404 error message.

---
+++ src/app/(shop)/category/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function ({ params }: Props) {
+  const { id } = params;
+
+  if (id === 'kids') {
+    notFound();
+  }
+
+  return (
+    <div>
+      <h1>Category Page {id}</h1>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './ui/top-menu/TopMenu';
+export * from './ui/not-found/PageNotFound';
