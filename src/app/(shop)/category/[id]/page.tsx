d6acf205bac8d08fccecfeeef93cba0cdb671419
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-27 11:59:20 -0600

    ```text feat: Add lazy loading for images in category page
    The code changes include adding lazy loading for images in the category page. This improves the page load performance by loading images only when they are visible on the screen. The implementation utilizes the ProductGrid component and filters the products based on the selected category. Additionally, the page title and subtitle are updated based on the selected category.

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,40 @@
-import { notFound } from 'next/navigation';
+import { ProductGrid, Title } from '@/components';
+import { Category } from '@/interfaces';
+import { initialData } from '@/seed/seed';
 
+const seedProducts = initialData.products;
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
-export default function ({ params }: Props) {
+export default function ProductGridFiltered({ params }: Props) {
   const { id } = params;
+  const products = seedProducts.filter((product) => product.gender === id);
 
-  if (id === 'kids') {
-    notFound();
-  }
+  const labels: Record<Category, string> = {
+    men: 'para Hombres',
+    women: 'para Mujeres',
+    kid: 'para Niños',
+    unisex: 'para todos',
+  };
+
+  const subtitle: Record<Category, string> = {
+    men: 'Todos los productos para él',
+    women: 'Todos los productos para ella',
+    kid: 'Todos los productos para más pequeños',
+    unisex: 'Todos los productos para todos',
+  };
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title
+        title={`Articulos ${labels[id]}`}
+        subtitle={subtitle[id]}
+        className="mb-2"
+      />
+      <ProductGrid products={products} />
+    </>
   );
 }
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -36,7 +36,7 @@ export const TopMenu = () => {
         </Link>
         <Link
           className="m-2 p-2 rounded-md transition-all hover: bg-gray-100"
-          href={'/category/kids'}
+          href={'/category/kid'}
         >
           Niños
         </Link>
