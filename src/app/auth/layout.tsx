99e6ffb95dc49bcc35409aa4e5fa5f90fe30b1fc
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-28 11:29:48 -0600

    feat: Update login and new account pages with improved layout and styling
    The code changes include updating the login and new account pages with a more user-friendly layout and styling. This improves the overall appearance and usability of the pages. The changes include adding labels and input fields for email and password, as well as a button for submitting the form. Additionally, a divider line and a link to create a new account are included. These updates enhance the user experience and make the login and new account process more intuitive.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,5 +3,9 @@ export default function ShopLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="min-h-screen bg-green-500">{children}</main>;
+  return (
+    <main className="flex justify-center">
+      <div className="w-full sm:w-[350px] px-10">{children}</div>
+    </main>
+  );
 }
