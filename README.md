b9193b5487743284e293fc164d3713e34178c327
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-28 20:52:59 -0600

    feat: Add 'L' size to enum 'Size'
    This commit adds the 'L' size to the 'Size' enum in the Prisma schema. This allows for the representation of larger sizes in the application. The change includes modifying the schema.prisma file and creating a new migration.sql file to alter the enum in the database. This update ensures that the 'L' size is available for selection and storage in the application.

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 3. Instalar dependencias `npm install`
 4. Levantar la base de datos `docker compose up -d`
 5. Correr las migraciones de Prisma `npx prisma migrate dev`
-6. Correr el proyecto `npm run dev`
+6. Ejecutar seed `npm run seed`
+7. Correr el proyecto `npm run dev`
 
 ## Correr en prod
---
+++ src/seed/seed-database.ts
@@ -0,0 +1,57 @@
+import { initialData } from './seed';
+import prisma from '../lib/prisma';
+
+async function main() {
+  // 1. Erase previous data
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  // 2. Categories
+  const categoriesData = categories.map((name) => ({ name }));
+
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>); // <string = shirt, string = categoryID>
+
+  // 3. Products
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // 4. Product Images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
+  console.log('Seed executed successfully');
+}
+
+(() => {
+  if (process.env.NODE_ENV === 'production') return;
+
+  main();
+})();
