a0f57d79c6d1e455b79a8b0bc1e30f08d17dfba8
Marco Rodriguez <marcotbcreator@gmail.com>
2024-05-29 23:46:47 -0600

    feat: Update page component names for better clarity

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -9,6 +9,7 @@ import {
   StockLabel,
 } from '@/components';
 import { titleFont } from '@/config/fonts';
+import { Metadata, ResolvingMetadata } from 'next';
 import { notFound } from 'next/navigation';
 
 interface Props {
@@ -16,6 +17,31 @@ interface Props {
     slug: string;
   };
 }
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug;
+
+  // fetch data
+  const product = await getProductBySlug(slug);
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product?.title ?? 'Producto no encontrado',
+    description: product?.description ?? '',
+    openGraph: {
+      title: product?.title ?? 'Producto no encontrado',
+      description: product?.description ?? '',
+      images: [`/products/${product?.images[1]}`],
+    },
+  };
+}
+
 export default async function ProductBySlug({ params }: Props) {
   const { slug } = params;
   const product = await getProductBySlug(slug);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import { inter } from '@/config/fonts';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Teslo | Shop',
+  title: {
+    template: '%s - Teslo | Shop',
+    default: 'Home - Teslo | Shop',
+  },
   description: 'Una tienda virtual de productos',
 };
 
