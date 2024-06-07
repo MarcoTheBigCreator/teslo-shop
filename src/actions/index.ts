670e67184d5df4d8ad3953d60139f0903ab80c96
Marco Rodriguez <marcotbcreator@gmail.com>
2024-06-07 11:50:18 -0600

    feat: Add change-user-role action to modify user role
    This commit adds the change-user-role action to the user module. The action allows for modifying the role of a user. It checks if the current user is an admin before updating the user's role in the database. Additionally, it triggers a revalidation of the '/admin/users' path to reflect the updated user role. This addition enhances the functionality of the application by providing a way for admins to change user roles.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,4 +21,5 @@ export * from './product/get-product-by-slug';
 export * from './product/get-stock-by-slug';
 export * from './product/product-pagination';
 
+export * from './user/change-user-role';
 export * from './user/get-paginated-users-admin';
--- src/app/(shop)/admin/users/ui/UsersTable.tsx
+++ src/app/(shop)/admin/users/ui/UsersTable.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { changeUserRole } from '@/actions';
 import { User } from '@/interfaces';
 
 interface Props {
@@ -55,7 +56,7 @@ export const UsersTable = ({ users }: Props) => {
             <td className="text-sm text-gray-900 font-light px-6 ">
               <select
                 value={user.role}
-                onChange={(e) => console.log(e.target.value)}
+                onChange={(e) => changeUserRole(user.id, e.target.value)}
                 className="text-sm text-gray-900 w-full p-2"
               >
                 <option value="admin">Admin</option>
