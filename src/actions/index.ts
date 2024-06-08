e8e8f0901df80ba9e18cd5fdd998594219673993
Marco Rodriguez <marcotbcreator@gmail.com>
2024-06-07 22:51:43 -0600

    ```text feat: Update module exports to use named exports
    This commit updates the module exports in the src/actions/index.ts file to use named exports instead of exporting everything with the wildcard operator. This change improves the clarity and maintainability of the code by explicitly specifying the exported functions.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,28 +1,28 @@
 // export * from './auth/logout';
 
-export * from './address/delete-user-address';
-export * from './address/get-user-address';
-export * from './address/set-user-address';
+export { deleteUserAddress } from './address/delete-user-address';
+export { getUserAddress } from './address/get-user-address';
+export { setUserAddress } from './address/set-user-address';
 
 export * from './auth/login';
-export * from './auth/register';
+export { registerUser } from './auth/register';
 
-export * from './country/get-countries';
+export { getCountries } from './country/get-countries';
 
-export * from './order/get-order-by-id';
-export * from './order/get-paginated-orders-by-user';
-export * from './order/get-paginated-orders-admin';
-export * from './order/place-order';
+export { getOrderById } from './order/get-order-by-id';
+export { getPaginatedOrdersByUser } from './order/get-paginated-orders-by-user';
+export { getPaginatedOrdersAdmin } from './order/get-paginated-orders-admin';
+export { placeOrder } from './order/place-order';
 
-export * from './payment/set-transaction-id';
-export * from './payment/paypal-check-payment';
+export { setTransactionId } from './payment/set-transaction-id';
+export { paypalCheckPayment } from './payment/paypal-check-payment';
 
-export * from './product/create-update-product';
-export * from './product/delete-product-image';
-export * from './product/get-product-by-slug';
-export * from './product/get-product-categories';
-export * from './product/get-stock-by-slug';
-export * from './product/product-pagination';
+export { createUpdateProduct } from './product/create-update-product';
+export { deleteProductImage } from './product/delete-product-image';
+export { getProductBySlug } from './product/get-product-by-slug';
+export { getProductCategories } from './product/get-product-categories';
+export { getStockBySlug } from './product/get-stock-by-slug';
+export { getPaginatedProductsWithImages } from './product/product-pagination';
 
-export * from './user/change-user-role';
-export * from './user/get-paginated-users-admin';
+export { changeUserRole } from './user/change-user-role';
+export { getPaginatedUsersAdmin } from './user/get-paginated-users-admin';
